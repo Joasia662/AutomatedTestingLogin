e478aa85e8e2ea5a75e5e8d43410a346835529e3
Joanna Cepuch <asiacepuch1@gmail.com>
2025-11-17 20:31:46 +0100

    clean auto-generated comments

--- test/pageobjects/dashboard.page.js
+++ test/pageobjects/dashboard.page.js
@@ -1,21 +1,12 @@
 import { $ } from '@wdio/globals'
 import Page from './page.js';
 
-/**
- * sub page containing specific selectors and methods for a specific page
- */
+
 class DashboardPage extends Page {
-    /**
-     * define selectors using getter methods
-     */
-    get dashboardTitle () {
+    get dashboardTitle() {
         return $('div.app_logo');
     }
-
-    /**
-     * overwrite specific options to adapt it to page object
-     */
-    open () {
+    open() {
         return super.open('inventory.html');
     }
 }
